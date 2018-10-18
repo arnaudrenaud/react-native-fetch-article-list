552a9b8d8c4f387f5c5c2f802b858163607205ed
Arnaud Renaud <arnaud.renaud@gmail.com>
2018-10-18 15:32:33 +0200

    Add more example data

--- exampleData.js
+++ exampleData.js
@@ -6,6 +6,13 @@ const articles = [
     description:
       'The `jest` object is automatically in scope within every test file.',
   },
+  {
+    url: 'https://uxplanet.org/tabs-for-mobile-ux-design-d4cc4d9410d1',
+    thumbnailImageUrl:
+      'https://cdn-images-1.medium.com/max/1200/1*MACsg71sE9vfhT8D2M1Cng.jpeg',
+    title: 'Tabs for Mobile UX Design – UX Planet',
+    description: 'by Nick Babich',
+  },
 ];
 
 export default articles;
